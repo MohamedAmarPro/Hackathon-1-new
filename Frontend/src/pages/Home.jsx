bb161856a3a71763ac20a1e6ab419126e6926347
Romain-Constant <romain.constant59@gmail.com>
2023-05-11 15:13:52 +0200

    footer done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Home.module.css";
 
 function Home() {
-  return <div className={styles.home}>Apparts</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.appartmentsContainer}></div>
+    </div>
+  );
 }
 
 export default Home;
