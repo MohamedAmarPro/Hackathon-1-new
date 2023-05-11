281172c2ace090bd3bef14f1ad00cc374f52223a
Romain-Constant <romain.constant59@gmail.com>
2023-05-11 17:23:43 +0200

    swiper done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import styles from "./Home.module.css";
 import AppartmentCard from "../components/appartmentCard/AppartmentCard";
 import appartmentsData from "../data/data.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.appartmentsContainer}>
-        {appartmentsData.map((appartment) => (
-          <AppartmentCard key={appartment.id} appartment={appartment} />
-        ))}
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={20}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {appartmentsData.map((appartment) => (
+            <SwiperSlide>
+              <AppartmentCard key={appartment.id} appartment={appartment} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
