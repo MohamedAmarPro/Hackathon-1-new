03cfab69fdc10ca165391c8af65b988b3338e9e2
Romain-Constant <romain.constant59@gmail.com>
2023-05-11 16:12:33 +0200

    appartment card done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./Home.module.css";
+import AppartmentCard from "../components/appartmentCard/AppartmentCard";
+import appartmentsData from "../data/data.json";
 
 function Home() {
   return (
     <div className={styles.home}>
-      <div className={styles.appartmentsContainer}></div>
+      <div className={styles.appartmentsContainer}>
+        {appartmentsData.map((appartment) => (
+          <AppartmentCard key={appartment.id} appartment={appartment} />
+        ))}
+      </div>
     </div>
   );
 }
